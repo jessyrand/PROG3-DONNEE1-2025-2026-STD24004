4af2dd5261b4760b19784a1deb8537163a051ab6
jessyrand <henintsoarandriam@gmail.com>
2025-11-15 14:21:35 +0300

    2nd question

--- team.sql
+++ team.sql
@@ -1,1 +1,3 @@
-SELECT id, first_name, last_name FROM employee WHERE team_id IS NULL
+SELECT id, first_name, last_name FROM employee WHERE team_id IS NULL;
+
+SELECT employee.id, employee.first_name, employee.last_name FROM employee LEFT JOIN leave ON leave.employee_id = employee.id WHERE leave.id IS NULL;
